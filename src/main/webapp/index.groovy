74c000231fa3b763a1c9f3ad6a89607aec4e6c27
Joe McCall <joemccall@intrepid-networks.com>
2012-10-17 11:26:28 -0400

    Use the html builder instead of raw html. Commented out test version of mongodb. Use the welcome-file list to run the app for root.

--- src/main/webapp/index.groovy
+++ src/main/webapp/index.groovy
@@ -26,20 +26,6 @@ texts = mongoDbFactory.db.getCollection('texts')
 charlist = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9','0','`',',','.','/',';','\'','-','=','[',']','\\']
 countmap = [:]
 
-// TODO This could probably be cleaner using html builder, but this works for
-// now
-println """
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	
-	<title>Online Keyboard</title>
-	<link rel="stylesheet" type="text/css" href="css/style.css" />
-</head>
-<body>
-"""
-
 // We assign a negative value to this to simplify sorting. We want the
 // higher-values to show up first. The ideal scenario is to do a custom sort to
 // reverse the list.
@@ -51,83 +37,150 @@ charlist.each {
 // sort by value
 countmap = countmap.sort { it.value }
 
+// Collect the keys
 sortedChars = countmap.collect { it.key }
 
-println """
-<div id="container">
-	<ul id="keyboard">
-		<li class="symbol"><span class="off">${sortedChars[34]}</span><span class="on">~</span></li>
-		<li class="symbol"><span class="off">${sortedChars[35]}</span><span class="on">!</span></li>
-		<li class="symbol"><span class="off">${sortedChars[36]}</span><span class="on">@</span></li>
-		<li class="symbol"><span class="off">${sortedChars[37]}</span><span class="on">#</span></li>
-		<li class="symbol"><span class="off">${sortedChars[38]}</span><span class="on">\$</span></li>
-		<li class="symbol"><span class="off">${sortedChars[39]}</span><span class="on">%</span></li>
-		<li class="symbol"><span class="off">${sortedChars[40]}</span><span class="on">^</span></li>
-		<li class="symbol"><span class="off">${sortedChars[41]}</span><span class="on">&amp;</span></li>
-		<li class="symbol"><span class="off">${sortedChars[42]}</span><span class="on">*</span></li>
-		<li class="symbol"><span class="off">${sortedChars[43]}</span><span class="on">(</span></li>
-		<li class="symbol"><span class="off">${sortedChars[44]}</span><span class="on">)</span></li>
-		<li class="symbol"><span class="off">${sortedChars[45]}</span><span class="on">_</span></li>
-		<li class="symbol"><span class="off">${sortedChars[46]}</span><span class="on">+</span></li>
-		<li class="delete lastitem">delete</li>
-		<li class="tab">tab</li>
-		<li class="letter">${sortedChars[23]}</li>
-		<li class="letter">${sortedChars[21]}</li>
-		<li class="letter">${sortedChars[19]}</li>
-		<li class="letter">${sortedChars[17]}</li>
-		<li class="letter">${sortedChars[15]}</li>
-		<li class="letter">${sortedChars[10]}</li>
-		<li class="letter">${sortedChars[12]}</li>
-		<li class="letter">${sortedChars[16]}</li>
-		<li class="letter">${sortedChars[18]}</li>
-		<li class="letter">${sortedChars[20]}</li>
-		<li class="symbol"><span class="off">${sortedChars[31]}</span><span class="on">{</span></li>
-		<li class="symbol"><span class="off">${sortedChars[32]}</span><span class="on">}</span></li>
-		<li class="symbol lastitem"><span class="off">${sortedChars[33]}</span><span class="on">|</span></li>
-		<li class="capslock">caps lock</li>
-		<li class="letter">${sortedChars[7]}</li>
-		<li class="letter">${sortedChars[5]}</li>
-		<li class="letter">${sortedChars[3]}</li>
-		<li class="letter">${sortedChars[1]}</li>
-		<li class="letter">${sortedChars[9]}</li>
-		<li class="letter">${sortedChars[8]}</li>
-		<li class="letter">${sortedChars[0]}</li>
-		<li class="letter">${sortedChars[2]}</li>
-		<li class="letter">${sortedChars[4]}</li>
-		<li class="symbol"><span class="off">${sortedChars[6]}</span><span class="on">:</span></li>
-		<li class="symbol"><span class="off">${sortedChars[30]}</span><span class="on">&quot;</span></li>
-		<li class="return lastitem">return</li>
-		<li class="left-shift">shift</li>
-		<li class="letter">${sortedChars[29]}</li>
-		<li class="letter">${sortedChars[27]}</li>
-		<li class="letter">${sortedChars[25]}</li>
-		<li class="letter">${sortedChars[13]}</li>
-		<li class="letter">${sortedChars[11]}</li>
-		<li class="letter">${sortedChars[14]}</li>
-		<li class="letter">${sortedChars[22]}</li>
-		<li class="symbol"><span class="off">${sortedChars[24]}</span><span class="on">&lt;</span></li>
-		<li class="symbol"><span class="off">${sortedChars[26]}</span><span class="on">&gt;</span></li>
-		<li class="symbol"><span class="off">${sortedChars[28]}</span><span class="on">?</span></li>
-		<li class="right-shift lastitem">shift</li>
-		<li class="space lastitem">&nbsp;</li>
-	</ul>
-</div>
+// Render the HTML
+html.html {
+    head {
+        title "Keyboredom Automated Keyboard Generator"
+        link(rel: "stylesheet", type: "text/css", href: "css/style.css")
+    }
 
-"""
+    body {
+        // This is hard-coded here because order is important. Otherwise I
+        // would loop over the list to create this.
+        ul (id: "keyboard") {
+            // Number Row
+            li (class: "symbol", sortedChars[34])
+            li (class: "symbol", sortedChars[35])
+            li (class: "symbol", sortedChars[36])
+            li (class: "symbol", sortedChars[37])
+            li (class: "symbol", sortedChars[38])
+            li (class: "symbol", sortedChars[39])
+            li (class: "symbol", sortedChars[40])
+            li (class: "symbol", sortedChars[41])
+            li (class: "symbol", sortedChars[42])
+            li (class: "symbol", sortedChars[43])
+            li (class: "symbol", sortedChars[44])
+            li (class: "symbol", sortedChars[45])
+            li (class: "symbol", sortedChars[46])
 
-println "<strong>Alphabetical Order:</strong><br/>"
-charlist.each {
-    println "\'${it}\': ${countmap[it]}   "
+            li (class:"delete lastitem", "delete")
+            li (class:"tab", "tab")
+
+            // Top Row
+            li (class: "letter", sortedChars[23])
+            li (class: "letter", sortedChars[21])
+            li (class: "letter", sortedChars[19])
+            li (class: "letter", sortedChars[17])
+            li (class: "letter", sortedChars[15])
+            li (class: "letter", sortedChars[10])
+            li (class: "letter", sortedChars[12])
+            li (class: "letter", sortedChars[16])
+            li (class: "letter", sortedChars[18])
+            li (class: "letter", sortedChars[20])
+            li (class: "symbol", sortedChars[31])
+            li (class: "symbol", sortedChars[32])
+            li (class: "symbol lastitem", sortedChars[33])
+            li (class: "capslock", "caps lock")
+
+            // Home Row (note the low numbers)
+            li (class: "letter", sortedChars[7])
+            li (class: "letter", sortedChars[5])
+            li (class: "letter", sortedChars[3])
+            li (class: "letter", sortedChars[1])
+            li (class: "letter", sortedChars[9])
+            li (class: "letter", sortedChars[8])
+            li (class: "letter", sortedChars[0])
+            li (class: "letter", sortedChars[2])
+            li (class: "letter", sortedChars[4])
+            li (class: "symbol", sortedChars[6])
+            li (class: "symbol", sortedChars[30])
+            li (class: "return lastitem", "return")
+
+            li (class: "left-shift", "shift")
+            li (class: "letter", sortedChars[29])
+            li (class: "letter", sortedChars[27])
+            li (class: "letter", sortedChars[25])
+            li (class: "letter", sortedChars[13])
+            li (class: "letter", sortedChars[11])
+            li (class: "letter", sortedChars[14])
+            li (class: "letter", sortedChars[22])
+            li (class: "symbol", sortedChars[24])
+            li (class: "symbol", sortedChars[26])
+            li (class: "symbol", sortedChars[28])
+            li (class: "right-shift lastitem", "shift")
+            li (class: "space lastitem", " ")
+        }
+
+        strong "Alphabetical Order:" 
+        p charlist.collect { "\'${it}\': ${countmap[it]}" }.join(" ")
+        
+        strong "Sorted:" 
+        p countmap.collect { "\'${it.key}\': ${-it.value}" }.join(" ")
+    }
 }
-println "<br/>"
 
-println "<strong>Sorted:</strong><br/>"
-countmap.each {
-    println "\'${it.key}\': ${-it.value}   "
-}
+//println """
+//<div id="container">
+	//<ul id="keyboard">
+		//<li class="symbol"><span class="off">${sortedChars[34]}</span><span class="on">~</span></li>
+		//<li class="symbol"><span class="off">${sortedChars[35]}</span><span class="on">!</span></li>
+		//<li class="symbol"><span class="off">${sortedChars[36]}</span><span class="on">@</span></li>
+		//<li class="symbol"><span class="off">${sortedChars[37]}</span><span class="on">#</span></li>
+		//<li class="symbol"><span class="off">${sortedChars[38]}</span><span class="on">\$</span></li>
+		//<li class="symbol"><span class="off">${sortedChars[39]}</span><span class="on">%</span></li>
+		//<li class="symbol"><span class="off">${sortedChars[40]}</span><span class="on">^</span></li>
+		//<li class="symbol"><span class="off">${sortedChars[41]}</span><span class="on">&amp;</span></li>
+		//<li class="symbol"><span class="off">${sortedChars[42]}</span><span class="on">*</span></li>
+		//<li class="symbol"><span class="off">${sortedChars[43]}</span><span class="on">(</span></li>
+		//<li class="symbol"><span class="off">${sortedChars[44]}</span><span class="on">)</span></li>
+		//<li class="symbol"><span class="off">${sortedChars[45]}</span><span class="on">_</span></li>
+		//<li class="symbol"><span class="off">${sortedChars[46]}</span><span class="on">+</span></li>
+		//<li class="delete lastitem">delete</li>
+		//<li class="tab">tab</li>
+		//<li class="letter">${sortedChars[23]}</li>
+		//<li class="letter">${sortedChars[21]}</li>
+		//<li class="letter">${sortedChars[19]}</li>
+		//<li class="letter">${sortedChars[17]}</li>
+		//<li class="letter">${sortedChars[15]}</li>
+		//<li class="letter">${sortedChars[10]}</li>
+		//<li class="letter">${sortedChars[12]}</li>
+		//<li class="letter">${sortedChars[16]}</li>
+		//<li class="letter">${sortedChars[18]}</li>
+		//<li class="letter">${sortedChars[20]}</li>
+		//<li class="symbol"><span class="off">${sortedChars[31]}</span><span class="on">{</span></li>
+		//<li class="symbol"><span class="off">${sortedChars[32]}</span><span class="on">}</span></li>
+		//<li class="symbol lastitem"><span class="off">${sortedChars[33]}</span><span class="on">|</span></li>
+		//<li class="capslock">caps lock</li>
+		//<li class="letter">${sortedChars[7]}</li>
+		//<li class="letter">${sortedChars[5]}</li>
+		//<li class="letter">${sortedChars[3]}</li>
+		//<li class="letter">${sortedChars[1]}</li>
+		//<li class="letter">${sortedChars[9]}</li>
+		//<li class="letter">${sortedChars[8]}</li>
+		//<li class="letter">${sortedChars[0]}</li>
+		//<li class="letter">${sortedChars[2]}</li>
+		//<li class="letter">${sortedChars[4]}</li>
+		//<li class="symbol"><span class="off">${sortedChars[6]}</span><span class="on">:</span></li>
+		//<li class="symbol"><span class="off">${sortedChars[30]}</span><span class="on">&quot;</span></li>
+		//<li class="return lastitem">return</li>
+		//<li class="left-shift">shift</li>
+		//<li class="letter">${sortedChars[29]}</li>
+		//<li class="letter">${sortedChars[27]}</li>
+		//<li class="letter">${sortedChars[25]}</li>
+		//<li class="letter">${sortedChars[13]}</li>
+		//<li class="letter">${sortedChars[11]}</li>
+		//<li class="letter">${sortedChars[14]}</li>
+		//<li class="letter">${sortedChars[22]}</li>
+		//<li class="symbol"><span class="off">${sortedChars[24]}</span><span class="on">&lt;</span></li>
+		//<li class="symbol"><span class="off">${sortedChars[26]}</span><span class="on">&gt;</span></li>
+		//<li class="symbol"><span class="off">${sortedChars[28]}</span><span class="on">?</span></li>
+		//<li class="right-shift lastitem">shift</li>
+		//<li class="space lastitem">&nbsp;</li>
+	//</ul>
+//</div>
 
-println """
-</body>
-</html>
-"""
+//"""
 
