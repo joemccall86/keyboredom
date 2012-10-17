23ee78d48120358f55e8bdf0b58e487e0c883abd
Joe McCall <joemccall@intrepid-networks.com>
2012-10-17 12:15:33 -0400

    Style css modifications. Added text explaining what it is. Added link to source code.

--- src/main/webapp/index.groovy
+++ src/main/webapp/index.groovy
@@ -48,139 +48,104 @@ html.html {
     }
 
     body {
-        // This is hard-coded here because order is important. Otherwise I
-        // would loop over the list to create this.
-        ul (id: "keyboard") {
-            // Number Row
-            li (class: "symbol", sortedChars[34])
-            li (class: "symbol", sortedChars[35])
-            li (class: "symbol", sortedChars[36])
-            li (class: "symbol", sortedChars[37])
-            li (class: "symbol", sortedChars[38])
-            li (class: "symbol", sortedChars[39])
-            li (class: "symbol", sortedChars[40])
-            li (class: "symbol", sortedChars[41])
-            li (class: "symbol", sortedChars[42])
-            li (class: "symbol", sortedChars[43])
-            li (class: "symbol", sortedChars[44])
-            li (class: "symbol", sortedChars[45])
-            li (class: "symbol", sortedChars[46])
+        div (id: "Content") {
+            h1 "Keyboredom Automatic Keyboard Generator"
+                p """
+                What if programmers could use a new keyboard layout and immediately
+                became up-to-speed with it? In such a scenario the most used keys would
+                need to be relocated to obtain optimum efficiency.
+                """
 
-            li (class:"delete lastitem", "delete")
-            li (class:"tab", "tab")
+                p """
+                This webapp looks at the twitter feed from @SpringSource, GitHub, and
+                the spring source blog and generates an optimal keyboard layout for
+                that information to be reproduced with minimal finger movement. For
+                example, the most used letter would be remapped to the right index
+                finger in the home row.
+                """
 
-            // Top Row
-            li (class: "letter", sortedChars[23])
-            li (class: "letter", sortedChars[21])
-            li (class: "letter", sortedChars[19])
-            li (class: "letter", sortedChars[17])
-            li (class: "letter", sortedChars[15])
-            li (class: "letter", sortedChars[10])
-            li (class: "letter", sortedChars[12])
-            li (class: "letter", sortedChars[16])
-            li (class: "letter", sortedChars[18])
-            li (class: "letter", sortedChars[20])
-            li (class: "symbol", sortedChars[31])
-            li (class: "symbol", sortedChars[32])
-            li (class: "symbol lastitem", sortedChars[33])
-            li (class: "capslock", "caps lock")
+                p """
+                Refresh your browser for updated data
+                """
 
-            // Home Row (note the low numbers)
-            li (class: "letter", sortedChars[7])
-            li (class: "letter", sortedChars[5])
-            li (class: "letter", sortedChars[3])
-            li (class: "letter", sortedChars[1])
-            li (class: "letter", sortedChars[9])
-            li (class: "letter", sortedChars[8])
-            li (class: "letter", sortedChars[0])
-            li (class: "letter", sortedChars[2])
-            li (class: "letter", sortedChars[4])
-            li (class: "symbol", sortedChars[6])
-            li (class: "symbol", sortedChars[30])
-            li (class: "return lastitem", "return")
+                // This is hard-coded here because order is important.
+                // Otherwise I would loop over the list to create this. The
+                // semi-colons are there for vim formatting
+                ul (id: "keyboard") {
+                    // Number Row
+                    li (class: "symbol", sortedChars[34]);
+                    li (class: "symbol", sortedChars[35]);
+                    li (class: "symbol", sortedChars[36]);
+                    li (class: "symbol", sortedChars[37]);
+                    li (class: "symbol", sortedChars[38]);
+                    li (class: "symbol", sortedChars[39]);
+                    li (class: "symbol", sortedChars[40]);
+                    li (class: "symbol", sortedChars[41]);
+                    li (class: "symbol", sortedChars[42]);
+                    li (class: "symbol", sortedChars[43]);
+                    li (class: "symbol", sortedChars[44]);
+                    li (class: "symbol", sortedChars[45]);
+                    li (class: "symbol", sortedChars[46]);
+                   
+                    li (class:"delete lastitem", "delete");
+                    li (class:"tab", "tab");
+                    
+                    // Top Row;
+                    li (class: "letter", sortedChars[23]);
+                    li (class: "letter", sortedChars[21]);
+                    li (class: "letter", sortedChars[19]);
+                    li (class: "letter", sortedChars[17]);
+                    li (class: "letter", sortedChars[15]);
+                    li (class: "letter", sortedChars[10]);
+                    li (class: "letter", sortedChars[12]);
+                    li (class: "letter", sortedChars[16]);
+                    li (class: "letter", sortedChars[18]);
+                    li (class: "letter", sortedChars[20]);
+                    li (class: "symbol", sortedChars[31]);
+                    li (class: "symbol", sortedChars[32]);
+                    li (class: "symbol lastitem", sortedChars[33]);
+                    li (class: "capslock", "caps lock");
 
-            li (class: "left-shift", "shift")
-            li (class: "letter", sortedChars[29])
-            li (class: "letter", sortedChars[27])
-            li (class: "letter", sortedChars[25])
-            li (class: "letter", sortedChars[13])
-            li (class: "letter", sortedChars[11])
-            li (class: "letter", sortedChars[14])
-            li (class: "letter", sortedChars[22])
-            li (class: "symbol", sortedChars[24])
-            li (class: "symbol", sortedChars[26])
-            li (class: "symbol", sortedChars[28])
-            li (class: "right-shift lastitem", "shift")
-            li (class: "space lastitem", " ")
+                    // Home Row (note the low numbers);
+                    li (class: "letter", sortedChars[7]);
+                    li (class: "letter", sortedChars[5]);
+                    li (class: "letter", sortedChars[3]);
+                    li (class: "letter", sortedChars[1]);
+                    li (class: "letter", sortedChars[9]);
+                    li (class: "letter", sortedChars[8]);
+                    li (class: "letter", sortedChars[0]);
+                    li (class: "letter", sortedChars[2]);
+                    li (class: "letter", sortedChars[4]);
+                    li (class: "symbol", sortedChars[6]);
+                    li (class: "symbol", sortedChars[30]);
+                    li (class: "return lastitem", "return");
+
+                    li (class: "left-shift", "shift");
+                    li (class: "letter", sortedChars[29]);
+                    li (class: "letter", sortedChars[27]);
+                    li (class: "letter", sortedChars[25]);
+                    li (class: "letter", sortedChars[13]);
+                    li (class: "letter", sortedChars[11]);
+                    li (class: "letter", sortedChars[14]);
+                    li (class: "letter", sortedChars[22]);
+                    li (class: "symbol", sortedChars[24]);
+                    li (class: "symbol", sortedChars[26]);
+                    li (class: "symbol", sortedChars[28]);
+                    li (class: "right-shift lastitem", "shift");
+                    li (class: "space lastitem", " ");
+                }
+
+            div {
+                strong "Alphabetical Order:" ;
+                p charlist.collect { "\'${it}\': ${countmap[it]}" }.join(" ")
+
+                strong "Sorted:" 
+                p countmap.collect { "\'${it.key}\': ${-it.value}" }.join(" ")
+
+                p "The source code can be found here:"
+                a(href: "https://github.com/joemccall86/keyboredom", "Keyboredom on GitHub")
+            }
         }
-
-        strong "Alphabetical Order:" 
-        p charlist.collect { "\'${it}\': ${countmap[it]}" }.join(" ")
-        
-        strong "Sorted:" 
-        p countmap.collect { "\'${it.key}\': ${-it.value}" }.join(" ")
     }
 }
-
-//println """
-//<div id="container">
-	//<ul id="keyboard">
-		//<li class="symbol"><span class="off">${sortedChars[34]}</span><span class="on">~</span></li>
-		//<li class="symbol"><span class="off">${sortedChars[35]}</span><span class="on">!</span></li>
-		//<li class="symbol"><span class="off">${sortedChars[36]}</span><span class="on">@</span></li>
-		//<li class="symbol"><span class="off">${sortedChars[37]}</span><span class="on">#</span></li>
-		//<li class="symbol"><span class="off">${sortedChars[38]}</span><span class="on">\$</span></li>
-		//<li class="symbol"><span class="off">${sortedChars[39]}</span><span class="on">%</span></li>
-		//<li class="symbol"><span class="off">${sortedChars[40]}</span><span class="on">^</span></li>
-		//<li class="symbol"><span class="off">${sortedChars[41]}</span><span class="on">&amp;</span></li>
-		//<li class="symbol"><span class="off">${sortedChars[42]}</span><span class="on">*</span></li>
-		//<li class="symbol"><span class="off">${sortedChars[43]}</span><span class="on">(</span></li>
-		//<li class="symbol"><span class="off">${sortedChars[44]}</span><span class="on">)</span></li>
-		//<li class="symbol"><span class="off">${sortedChars[45]}</span><span class="on">_</span></li>
-		//<li class="symbol"><span class="off">${sortedChars[46]}</span><span class="on">+</span></li>
-		//<li class="delete lastitem">delete</li>
-		//<li class="tab">tab</li>
-		//<li class="letter">${sortedChars[23]}</li>
-		//<li class="letter">${sortedChars[21]}</li>
-		//<li class="letter">${sortedChars[19]}</li>
-		//<li class="letter">${sortedChars[17]}</li>
-		//<li class="letter">${sortedChars[15]}</li>
-		//<li class="letter">${sortedChars[10]}</li>
-		//<li class="letter">${sortedChars[12]}</li>
-		//<li class="letter">${sortedChars[16]}</li>
-		//<li class="letter">${sortedChars[18]}</li>
-		//<li class="letter">${sortedChars[20]}</li>
-		//<li class="symbol"><span class="off">${sortedChars[31]}</span><span class="on">{</span></li>
-		//<li class="symbol"><span class="off">${sortedChars[32]}</span><span class="on">}</span></li>
-		//<li class="symbol lastitem"><span class="off">${sortedChars[33]}</span><span class="on">|</span></li>
-		//<li class="capslock">caps lock</li>
-		//<li class="letter">${sortedChars[7]}</li>
-		//<li class="letter">${sortedChars[5]}</li>
-		//<li class="letter">${sortedChars[3]}</li>
-		//<li class="letter">${sortedChars[1]}</li>
-		//<li class="letter">${sortedChars[9]}</li>
-		//<li class="letter">${sortedChars[8]}</li>
-		//<li class="letter">${sortedChars[0]}</li>
-		//<li class="letter">${sortedChars[2]}</li>
-		//<li class="letter">${sortedChars[4]}</li>
-		//<li class="symbol"><span class="off">${sortedChars[6]}</span><span class="on">:</span></li>
-		//<li class="symbol"><span class="off">${sortedChars[30]}</span><span class="on">&quot;</span></li>
-		//<li class="return lastitem">return</li>
-		//<li class="left-shift">shift</li>
-		//<li class="letter">${sortedChars[29]}</li>
-		//<li class="letter">${sortedChars[27]}</li>
-		//<li class="letter">${sortedChars[25]}</li>
-		//<li class="letter">${sortedChars[13]}</li>
-		//<li class="letter">${sortedChars[11]}</li>
-		//<li class="letter">${sortedChars[14]}</li>
-		//<li class="letter">${sortedChars[22]}</li>
-		//<li class="symbol"><span class="off">${sortedChars[24]}</span><span class="on">&lt;</span></li>
-		//<li class="symbol"><span class="off">${sortedChars[26]}</span><span class="on">&gt;</span></li>
-		//<li class="symbol"><span class="off">${sortedChars[28]}</span><span class="on">?</span></li>
-		//<li class="right-shift lastitem">shift</li>
-		//<li class="space lastitem">&nbsp;</li>
-	//</ul>
-//</div>
-
-//"""
 
